562f5ae16c34f1c40da160f4ae69946e67913593
twonumberfortyfives <sma4no.7@gmail.com>
2025-09-28 22:22:10 +0300

    refactor: implement MSAL authentication in AppComponent

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,16 +1,79 @@
-import { Component } from '@angular/core';
-import { isIE } from '../../app.module';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+
+// Required for MSAL
+import { 
+  MsalService, 
+  MsalBroadcastService, 
+  MSAL_GUARD_CONFIG, 
+  MsalGuardConfiguration 
+} from '@azure/msal-angular';
+import { 
+  EventMessage, 
+  EventType, 
+  InteractionStatus, 
+  RedirectRequest 
+} from '@azure/msal-browser';
+
+// Required for RJXS
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
+
 
 @Component({
   standalone: false,
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{ // OnInit calls once when componennt is initialized, OnDestroy when moving from 
   title = 'msal-angular';
+  loginDisplay: boolean = false
+  tokenExpiration: string = ''
+  private readonly _destroying$ = new Subject<void>()
 
-  constructor(){
-    console.log(isIE)
-    console.log(window.navigator.userAgent)
+  constructor(
+    @Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
+    private authService: MsalService,
+    private msalBroadcastService: MsalBroadcastService,
+  ){}
+
+  setLoginDisplay() {
+    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0
   }
+
+  ngOnInit(): void {
+    this.msalBroadcastService.inProgress$
+      .pipe(
+        filter((status: InteractionStatus) => status === InteractionStatus.None),
+        takeUntil(this._destroying$)
+      )
+      .subscribe(() => {
+        this.setLoginDisplay()
+      })
+    
+    this.msalBroadcastService.msalSubject$.pipe(
+      filter(
+        (msg: EventMessage) => msg.eventType === EventType.ACQUIRE_TOKEN_SUCCESS)
+      ).subscribe(msg => {
+        this.tokenExpiration = (msg.payload as any).expiresOn
+    })
+    localStorage.setItem("tokenExpiration", this.tokenExpiration)
+  }
+
+  ngOnDestroy(): void {
+    this._destroying$.next(undefined)
+    this._destroying$.complete()
+  }
+
+  login() {
+    if (this.msalGuardConfig.authRequest) {
+      this.authService.loginRedirect({ ...this.msalGuardConfig.authRequest } as RedirectRequest)
+    } else {
+      this.authService.loginRedirect()
+    }
+  }
+
+  logout() {
+    this.authService.logoutRedirect()
+  }
+
 }
